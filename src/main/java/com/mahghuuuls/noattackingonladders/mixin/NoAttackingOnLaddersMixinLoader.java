a202d525c3def7e8d95860d6fbf81b2d138ece54
Mahghuuuls <arthcrs@gmail.com>
2026-06-18 23:30:43 -0300

    Register mixins early via CoreMod to prevent MixinTargetAlreadyLoadedException crash

---
+++ src/main/java/com/mahghuuuls/noattackingonladders/mixin/NoAttackingOnLaddersMixinLoader.java
@@ -0,0 +1,43 @@
+package com.mahghuuuls.noattackingonladders.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.Name("NoAttackingOnLaddersMixinLoader")
+public final class NoAttackingOnLaddersMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.noattackingonladders.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
